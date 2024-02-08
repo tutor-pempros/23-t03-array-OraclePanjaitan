ff8920d33e35548a5f100b63afb78f1d3522ff22
OraclePanjaitan <143679370+OraclePanjaitan@users.noreply.github.com>
2024-02-08 22:52:07 +0700

    12S23037 Oracle Tio Panjaitan

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,44 @@
-// NIM - NAMA
+// 12S23037 - Oracle Tio Panjaitan
 
 #include <stdio.h>
 
 int main(int _argc, char **_argv)
 {
 
-  // your code here
+  int n;
+
+  scanf("%d", &n);
+  int angka[n];
+  int smallest;
+  int biggest;
+
+
+  for(int i=0; i<n; i++)
+  {
+    scanf("%d", &angka[i]);
+    smallest = angka[i];
+    biggest = angka[i];
+  }
+
+  for(int i=0;i<n;i++)
+  {
+    if(angka[n]<smallest)
+    {
+      smallest = angka[n];      
+    }
+  }
+  for(int i=1;i<n;i++)
+  {
+    if(angka[i]> biggest)
+    {
+      biggest = angka[i];
+    }
+  }
+
+  
+
+  printf("%d", smallest);
+  printf("\n%d", biggest);
 
   return 0;
 }
--- t03_03.c
+++ t03_03.c
@@ -1,11 +1,54 @@
-// NIM - NAMA
+// 12S23037 - Oracle Tio Panjaitan
 
 #include <stdio.h>
 
 int main(int _argc, char **_argv)
 {
 
-  // your code here
+  int n;
+
+  scanf("%d", &n);
+  float angka[n];
+  float smallest;
+  float biggest;
+  float div;
+  float range;
+
+
+  for(int i=0; i<n; i++)
+  {
+    scanf("%f", &angka[i]);
+    smallest = angka[i];
+    biggest = angka[i];
+  }
+
+  for(int i=0;i<n;i++)
+  {
+    if(angka[n]<smallest)
+    {
+      smallest = angka[n];      
+    }
+  }
+  for(int i=1;i<n;i++)
+  {
+    if(angka[i]> biggest)
+    {
+      biggest = angka[i];
+    }
+  }
+
+  for(int i=0;i<n;i++)
+  {
+    div = div + angka[i];    
+  }
+  
+  div = div / n;
+  range = biggest + smallest;
+  
+  printf("%g", smallest);
+  printf("\n%g", biggest);
+  printf("\n%.2f", div);
+  printf("\n%g", range);
 
   return 0;
 }
