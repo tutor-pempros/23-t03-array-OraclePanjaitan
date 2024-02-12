04d994d43f842e5ef0ded9a3bdade4390c65c691
OraclePanjaitan <143679370+OraclePanjaitan@users.noreply.github.com>
2024-02-12 08:54:45 +0700

    12S23037 Oracle Tio Panjaitan

--- t03_03.c
+++ t03_03.c
@@ -43,7 +43,7 @@ int main(int _argc, char **_argv)
   }
   
   div = div / n;
-  range = biggest + smallest;
+  range = biggest - smallest;
   
   printf("%g", smallest);
   printf("\n%g", biggest);
